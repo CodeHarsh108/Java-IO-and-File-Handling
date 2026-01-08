2a130275275148765b856f2786d33054d1709b89
Harsh Patil <itsharshhh6@gmail.com>
2026-01-08 09:46:59 +0530

    File Creation

---
+++ src/FileClass.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+
+public class FileClass {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:/xampp/htdocs/movie_db/index.php");
+        if(file.exists()) System.out.println("Available!!!");
+
+        File file1 = new File("new.txt");
+        boolean created = file1.createNewFile();
+        if(created) System.out.println("New File Created");
+    }
+}
